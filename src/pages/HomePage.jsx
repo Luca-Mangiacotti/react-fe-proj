a903d23a892c8d6f3c386cb6c301096459accd26
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-07-22 11:53:04 +0200

    fix dir syntax

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Card from "../components/Ui/Card";
+import Card from "../components/ui/Card";
 
 export default function HomePage() {
   const [comics, setComics] = useState([]);
